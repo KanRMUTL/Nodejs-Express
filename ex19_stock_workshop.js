90a981b7745414ad083618934c82dace3a54f26b
kanRMUTL <kan4227@gmail.com>
2020-02-28 14:03:08 +0700

    workshop complete

--- ex19_stock_workshop.js
+++ ex19_stock_workshop.js
@@ -2,6 +2,7 @@ var express = require('express')
 var formidable = require('formidable')
 var path = require('path')
 var fs = require('fs')
+var sqlite3 = require('sqlite3')
 
 var app = express()
 var basePath = path.join(__dirname, '/stock')
@@ -11,21 +12,13 @@ app.set('views', './stock')
 app.use(express.static(basePath))
 
 app.get('/', function (req, res) {
-    var productDummy = []
-    for (let i = 0; i < 90; i++) {
-        productDummy.push({
-            id: 1,
-            img: 'poster.jpg',
-            title: 'Web Development',
-            description: 'Course descript',
-            price: 50000,
-            stock: 255
+    queryData(function (result) {
+        res.render('index', {
+            header: 'Stock Workshop',
+            products: result
         })
-    }
-    res.render('index', {
-        header: 'Stock Workshop',
-        products: productDummy
     })
+
 })
 
 
@@ -34,6 +27,16 @@ app.get('/add', function (req, res) {
         header: 'Create Product'
     })
 });
+
+app.get('/edit', function (req, res) {
+    queryById(req.query.id, function (row) {
+        res.render('edit', {
+            header: 'Edit Product',
+            item: row
+        })
+    })
+
+})
 
 app.post('/api/add', function (req, res) {
     try {
@@ -45,7 +48,7 @@ app.post('/api/add', function (req, res) {
             var newPath = path.join(__dirname, './stock/img/' + fileName)
 
             fs.rename(oldPath, newPath, function (error) {
-                if(error) throw error;
+                if (error) throw error;
             })
 
             var data = {
@@ -57,13 +60,153 @@ app.post('/api/add', function (req, res) {
             };
 
             console.log('data : ', data)
-            res.json(data)
+
+            insertData(data)
+            res.redirect('/')
         })
     } catch (error) {
         console.log('Err: ', +error)
         res.json(error)
     }
 })
+
+app.post('/api/update', function (req, res) {
+    try {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            if (files.fileupload.size != 0) {
+                var oldpath = files.fileupload.path
+                var newpath = path.join(__dirname, './stock/img/' + fields.image)
+
+                fs.rename(oldpath, newpath, function (err) {
+                    if (err) throw error;
+                    console.log('Update file successfully')
+                })
+
+            }
+            var data = {
+                id: fields.id,
+                title: fields.title,
+                description: fields.description,
+                price: fields.price,
+                stock: fields.stock,
+            };
+
+            console.log(JSON.stringify(data))
+            updateData(data)
+            res.redirect('/')
+        })
+    } catch (error) {
+        throw error
+    }
+})
+
+app.get('/api/delete', function (req, res) {
+    deleteData(req.query.id, function(){
+        res.redirect('/')
+    })
+})
+
+app.use(function(req, res, error){
+    console.log('error: ' + JSON.stringify(error))
+    res.sendFile(path.join(__dirname, './stock/404.html'))
+})
+
+function queryById(id, callback) {
+    let db = openDB();
+    db.get('SELECT * FROM stock where id=?', [id], (error, row) => {
+        if (error) {
+            throw error;
+        }
+        callback(row)
+    })
+    closeDB(db)
+}
+
+function queryData(callback) {
+    let db = openDB()
+    const sql = "SELECT * FROM stock ORDER BY id"
+    db.all(sql, function (error, rows) {
+        if (error) {
+            return callback([])
+        } else {
+            callback(rows)
+        }
+    })
+    closeDB(db)
+}
+
+function insertData(data) {
+    let db = openDB()
+    const sql = `
+        INSERT INTO stock(title,description,price,stock,image)
+        VALUES(
+            '${data.title}',
+            '${data.description}',
+            '${data.price}',
+            '${data.stock}',
+            '${data.image}'
+        )
+    `
+    console.log(sql)
+    db.run(sql)
+    closeDB(db)
+}
+
+function updateData(data) {
+    let db = openDB()
+    const sql = `
+        UPDATE stock SET
+        title = '${data.title}',
+        description = '${data.description}',
+        price = '${data.price}',
+        stock = '${data.stock}'
+        WHERE id = '${data.id}'
+    `
+
+    console.log(sql)
+
+    db.run(sql)
+
+    closeDB(db)
+}
+
+function deleteData(id, callback){
+    let db = openDB()
+    const sql = `DELETE FROM stock WHERE id = '${id}'`
+    db.run(sql, function (error) {
+        if(error) throw error
+        callback()
+    })
+}
+function openDB() {
+    let db = new sqlite3.Database(path.join(__dirname, '/stock/database/data.db'), (error) => {
+        if (error) {
+            console.error(error.message)
+        } else {
+            console.log('Connected to the stock database')
+        }
+    })
+
+    var sql_create_table = `CREATE TABLE IF NOT EXISTS 'stock' (
+            'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'title' TEXT,
+            'description' TEXT,
+            'price' INTEGER,
+            'stock' INTEGER,
+            'image' TEXT
+        )`
+    db.run(sql_create_table)
+    return db;
+}
+
+function closeDB(db) {
+    db.close((error) => {
+        if (error) {
+            return console.error(err.message)
+        }
+    })
+}
 
 
 var server = app.listen(3000, function () {
